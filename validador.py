9d7aa088316ee570db06e1a81cc49bacd3928d25
marcoskw <marcoskorbeswerle@hotmail.com>
2021-06-08 11:20:19 -0300

    Adicionando validador.py

---
+++ validador.py
@@ -0,0 +1,54 @@
+import re
+
+REGRESSIVOS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+
+def valida_cnpj(cnpj):
+    cnpj = apenas_numeros(cnpj)
+    try:
+        if sequencia(cnpj):
+            print('É uma sequência')
+            return False
+    except:
+        return False
+
+    try:
+        novo_cnpj = calcula_digito(cnpj=cnpj, digito=1)
+        novo_cnpj = calcula_digito(cnpj=novo_cnpj, digito=2)
+    except Exception as e:
+        return False
+
+    if novo_cnpj == cnpj:
+        return True
+    else:
+        return False
+
+
+def calcula_digito(cnpj, digito):
+    if digito == 1:
+        regressivos = REGRESSIVOS[1:]
+        novo_cnpj = cnpj[:-2]
+    elif digito == 2:
+        regressivos = REGRESSIVOS
+        novo_cnpj = cnpj
+    else:
+        return None
+
+    total = 0
+    for indice, regressivo in enumerate(regressivos):
+        total += int(cnpj[indice]) * regressivo
+    digito = 11 - (total % 11)
+    digito = digito if digito <= 9 else 0
+    return f'{novo_cnpj}{digito}'
+
+
+def sequencia(cnpj):
+    sequence = cnpj[0] * len(cnpj)
+    if sequence == cnpj:
+        return True
+    else:
+        return False
+
+
+def apenas_numeros(cpf):
+    return re.sub(r'[^0-9]', '', cpf)
